aef15af091e9e277979bccb36afc8dedb0b43344
shorox <skazo4nix@gmail.com>
2018-04-22 01:07:05 +0300

    lesson_1_and_2

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,12 +1,19 @@
 package hello;
 
+import hello.model.Message;
+import hello.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
@@ -14,4 +21,37 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping
+    public String main(Model model){
+        Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String addNewMessage (@RequestParam String text, @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter (@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        }else{
+            messages = messageRepository.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
 }
---
+++ src/main/java/hello/repository/MessageRepository.java
@@ -0,0 +1,10 @@
+package hello.repository;
+
+import hello.model.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
